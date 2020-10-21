fd323f2773d251698d27dbb7a9e967919ac046da
usman766 <muhammadusman12316@gmail.com>
2020-10-21 17:42:33 +0500

    doctor

---
+++ resources/views/vistor/viewdoctor.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Doctor</title>
+  </head>
+  <body>
+    <div class="container-fluid mt-4">
+         <div class="row justify-content-center align-items-center pt-3">
+          <!-- left column -->
+          <div class="col-md-10">
+    <div class="row">
+        <div class="form-group col-md-6 ">
+                    <label class="font-weight-bold">Select By Specialization</label>
+                    <select class="form-control" name="bname">
+                    <option value="" selected="">status</option>
+                   <option>A</option>
+                   <option>B</option>
+                   <option>C</option>
+                  </select>
+
+                  </div>
+        <div class="col-md-6">
+            <label class="font-weight-bold">Select By Doctor</label>
+          <select class="form-control">
+              <option selected="selected">Selected</option>
+              <option>A</option>
+              <option>B</option>
+              <option>C</option>
+          </select>  
+
+        </div>
+    </div>
+</div>
+</div>
+
+      <div class="row justify-content-center align-items-center pt-3">
+          <!-- left column -->
+          <div class="col-md-10">
+            <!-- general form elements -->
+            <div class="card shadow bg-white" style="border-radius: 10px">
+
+              
+                <div class="card-body ">
+                <div class="row">
+                <div class="col-md-9">
+                  <div class="row">
+                      <div class="col-md-2">
+                        <img src="{{asset('image/sr.png')}}" class="card-img-top" alt="...">
+
+                      </div>
+                      <div class="col-md-10">
+                          <h4>Dr. Samar Hussain</h4>
+                          <p>Gynecologist, Obstetrician</p>
+                          <p>M.B.B.S., FCPS (Gynecology & Obstetrics), MRCOG 1</p>
+                          <div class="row">
+                              <div class="col-md-4 border-right">
+                                  <h5>Under 15 Min</h5>
+                                  <p>Wait Time</p>
+                              </div>
+                             <div class="col-md-4 border-right">
+                                  <h5>Under 15 Min</h5>
+                                  <p>Wait Time</p>
+                              </div>
+                              <div class="col-md-4">
+                                  <h5>Under 15 Min</h5>
+                                  <p>Wait Time</p>
+                              </div>
+                          </div>
+                          <p>" very positive and mature approach towards issues....proper guidance. detailed analysis of issues. thanks dr samar..."</p>
+                      </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="row justify-content-center align-items-center">
+                  <a href="#" class="btn btn-primary">Video Consultation</a>
+                  <a href="#" class="btn btn-success mt-1">Book Appointment</a>
+              </div>
+                </div>
+                </div>
+                </div>
+                <!-- /.card-body -->
+
+                
+            
+            </div>
+          </div>
+        </div>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
